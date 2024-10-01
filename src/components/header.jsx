c2cf07db80ff0f7d47a0f2bf9a252703e4fcd2d8
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-01 12:42:06 -0500

    added header component

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+
+export default function Header() {
+    return (
+    <div>
+        <h1>Sticky Done</h1>
+        <h2>Organize, Track, and Complete Tasks!</h2>
+    </div>
+    )
+}
