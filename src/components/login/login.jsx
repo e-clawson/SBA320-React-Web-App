ba53a7e5a3af836e84913ead3bf735424f49642b
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-02 23:13:25 -0500

    added more styling to sign up form

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -7,26 +7,21 @@ import password_img from "../../assets/password.png"
 import person_img from "../../assets/person.png"
 
 export default function login({name, email, password}) {
-
-    //need to add some fuctions here to do the following: 
-    //1 - check that the email and password are in the correct format 
-    //2 - check if a user exists based on the input for email and password 
-    //AND that the password is correct for that user 
-    //3- login the user 
+    const [action, setAction] = useState("Sign Up")
 
     return (
     <div className="container">
         <div className="header">
-        <div className="text">Log-In</div>
+        <div className="text">{action}</div>
         </div>
-        <div className="signinForm">
-            <div className="input">
+        <div className="sign-in-form">
+            {action === "Log In" ? <div></div> :  <div className="input">
                 <img src={person_img} alt=""/>
                 <input
                 value={name}
                 placeholder="First Name"
                 ></input>
-            </div>
+            </div>}
             <div className="input">
                 <img src={email_img} alt=""/>
                 <input
@@ -42,24 +37,23 @@ export default function login({name, email, password}) {
                 ></input>
             </div>
         <div className="submit-container">
-            <div className="submit">Sign Up</div>
-            <div className="submit">Log In</div>
-            <div className="forgotPassword">Forgot Password? <span>Click Here</span></div>
-            <input
+            <div className={action === "Log In" ? "submit gray" : "submit"} onClick={() => {setAction("Sign Up")}}>Sign Up</div>
+            <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => {setAction("Log In")}}>Log In</div>
+            {/* <input
             type="button"
             value={'Log In'}
             className={'loginButton'}
-            >
-            </input>
+            > */}
+            {/* </input>
             <input
             type="button"
             value={'Sign Up'}
             className={'signUpButton'}
             >
-            </input>
+            </input> */}
         </div>
-        </div>
-        
+        </div> 
+        {action === "Sign Up" ? <div></div> : <div className="forgot-password">Forgot Password? <span>Click Here</span></div> }
     </div>
     )
 }
